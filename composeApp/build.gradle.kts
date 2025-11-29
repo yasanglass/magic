cf0ed5ddf4f40a33572e75568b6f340e65c210d7
Yasan Glass <yasanglass@gmail.com>
2025-11-29 12:33:28 +0100

    Apply formatting

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosArm64(),
         iosSimulatorArm64(),
@@ -27,20 +27,20 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     jvm()
-    
+
     js {
         browser()
         binaries.executable()
     }
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser()
         binaries.executable()
     }
-    
+
     sourceSets {
         androidMain.dependencies {
             implementation(compose.preview)
