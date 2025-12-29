9ed150778e6e19fe53a573f7ac7e5ec6387057d5
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-29 16:02:00 +0100

    Add basic animations

--- app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/route/magic/MagicScreen.kt
+++ app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/route/magic/MagicScreen.kt
@@ -1,18 +1,15 @@
 package glass.yasan.magic.presentation.route.magic
 
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.expandVertically
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
@@ -20,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -58,10 +56,12 @@ private fun MagicScreen(
 ) {
     val containerColor by animateColorAsState(state.answer.type.getContainerColor())
     val contentColor by animateColorAsState(containerColor.toContentColor())
+    val extraContentAlpha by animateFloatAsState(if (state.answer == Answer.empty) 1f else 0f)
+
     SystemBarColorsEffect(containerColor)
 
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = Modifier
             .fillMaxSize()
             .background(containerColor)
@@ -73,31 +73,32 @@ private fun MagicScreen(
                 sendEvent(Event.OnClick)
             },
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.weight(1f).fillMaxWidth().animateContentSize(),
-        ) {
+        AnimatedContent(
+            targetState = state.answer,
+            transitionSpec = { fadeIn() togetherWith fadeOut() },
+        ) { answer ->
             Text(
-                text = state.answer.getText(),
+                text = answer.getText(),
                 fontSize = 32.sp,
                 lineHeight = 32.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
                 color = contentColor,
                 modifier = Modifier
-                    .padding(32.dp),
+                    .padding(
+                        vertical = 128.dp,
+                        horizontal = 32.dp,
+                    ),
             )
         }
-        AnimatedVisibility(
-            visible = state.answer == Answer.empty,
-            enter = fadeIn() + expandVertically(),
-            exit = fadeOut() + shrinkVertically(),
-        ) {
-            ToolkitDeveloperLogoHorizontal(
-                color = contentColor,
-                modifier = Modifier.padding(32.dp).height(46.dp),
-            )
-        }
+        ToolkitDeveloperLogoHorizontal(
+            color = contentColor,
+            modifier = Modifier
+                .alpha(extraContentAlpha)
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+                .height(46.dp),
+        )
     }
 }
 
