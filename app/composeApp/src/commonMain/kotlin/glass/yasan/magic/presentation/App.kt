ac5a179b915621425dfe6a41e8a160fc556d4473
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-30 12:24:08 +0100

    Add in-memory theme settings

--- app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/App.kt
+++ app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/App.kt
@@ -1,17 +1,29 @@
 package glass.yasan.magic.presentation
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.compose.rememberNavController
 import glass.yasan.kepko.foundation.theme.KepkoTheme
+import glass.yasan.kepko.foundation.theme.ThemeStyle
 import glass.yasan.magic.presentation.navigation.NavigationHost
 
 @Composable
 fun App() {
     val navController = rememberNavController()
+    // TODO persist theme settings
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+    val themeStyle = rememberSaveable {
+        mutableStateOf(ThemeStyle.fromDarkMode(isDark = isSystemInDarkTheme))
+    }
 
-    KepkoTheme {
+    KepkoTheme(
+        style = themeStyle.value,
+    ) {
         NavigationHost(
             navController = navController,
+            themeStyle = themeStyle,
         )
     }
 }
--- app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/route/settings/SettingsScreen.kt
+++ app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/route/settings/SettingsScreen.kt
@@ -1,20 +1,27 @@
 package glass.yasan.magic.presentation.route.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import glass.yasan.kepko.component.ButtonText
+import glass.yasan.kepko.component.PreferenceRadioGroup
 import glass.yasan.kepko.component.Scaffold
 import glass.yasan.kepko.component.ic_chevron_forward
 import glass.yasan.kepko.foundation.theme.KepkoTheme
+import glass.yasan.kepko.foundation.theme.ThemeStyle
+import glass.yasan.kepko.util.asPreferenceRadioGroupItems
 import glass.yasan.magic.presentation.navigation.Route
 import glass.yasan.magic.presentation.util.SystemBarColorsEffect
 import glass.yasan.magic.resources.Res
 import glass.yasan.magic.resources.about
 import glass.yasan.magic.resources.settings
+import glass.yasan.magic.resources.theme
 import glass.yasan.toolkit.compose.spacer.verticalSpacerItem
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
@@ -23,6 +30,7 @@ import glass.yasan.kepko.component.Res as KepkoComponentRes
 @Composable
 fun SettingsScreen(
     navController: NavHostController,
+    themeStyle: MutableState<ThemeStyle>,
 ) {
     SystemBarColorsEffect(
         statusBarColor = KepkoTheme.colors.foreground,
@@ -34,9 +42,11 @@ fun SettingsScreen(
         onBackClick = { navController.navigateUp() },
     ) { contentPadding ->
         LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(12.dp),
             modifier = Modifier.padding(contentPadding),
         ) {
             verticalSpacerItem(height = 16.dp)
+            themePreference(themeStyle)
             item {
                 ButtonText(
                     text = stringResource(Res.string.about),
@@ -46,5 +56,16 @@ fun SettingsScreen(
                 )
             }
         }
+    }
+}
+
+private fun LazyListScope.themePreference(themeStyle: MutableState<ThemeStyle>) {
+    item {
+        PreferenceRadioGroup(
+            title = stringResource(Res.string.theme),
+            items = ThemeStyle.asPreferenceRadioGroupItems(),
+            selectedId = themeStyle.value.id,
+            onSelectId = { themeStyle.value = ThemeStyle.fromId(it) ?: ThemeStyle.LIGHT },
+        )
     }
 }
