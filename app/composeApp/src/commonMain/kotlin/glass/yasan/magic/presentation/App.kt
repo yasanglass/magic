102a36c41ea724e578052a4573374754e863e977
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-31 14:04:48 +0100

    Load content after setting finishes loading

--- app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/App.kt
+++ app/composeApp/src/commonMain/kotlin/glass/yasan/magic/presentation/App.kt
@@ -2,11 +2,17 @@ package glass.yasan.magic.presentation
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import glass.yasan.kepko.component.Surface
 import glass.yasan.kepko.foundation.theme.KepkoTheme
+import glass.yasan.magic.feature.settings.domain.model.Settings
 import glass.yasan.magic.feature.settings.domain.repository.SettingsRepository
 import glass.yasan.magic.presentation.navigation.NavigationHost
 import org.koin.compose.koinInject
@@ -17,19 +23,38 @@ fun App() {
     val settingsRepository: SettingsRepository = koinInject()
     val settings by settingsRepository.settings.collectAsStateWithLifecycle(initialValue = null)
 
-    settings?.let { nonNullSettings ->
+    App(
+        navController = navController,
+        settings = settings,
+        updateSettings = settingsRepository::update,
+    )
+}
+
+@Composable
+private fun App(
+    navController: NavHostController,
+    settings: Settings?,
+    updateSettings: (Settings.() -> Settings) -> Unit,
+) {
+    Surface(
+        color = Color.Black,
+        modifier = Modifier.fillMaxSize(),
+    ) {
         AnimatedVisibility(
             visible = settings != null,
             enter = fadeIn(),
+            modifier = Modifier.fillMaxSize(),
         ) {
-            KepkoTheme(
-                style = nonNullSettings.theme.asKepkoThemeStyle(),
-            ) {
-                NavigationHost(
-                    navController = navController,
-                    settings = nonNullSettings,
-                    updateSettings = settingsRepository::update,
-                )
+            settings?.let { nonNullSettings ->
+                KepkoTheme(
+                    style = nonNullSettings.theme.asKepkoThemeStyle(),
+                ) {
+                    NavigationHost(
+                        navController = navController,
+                        settings = nonNullSettings,
+                        updateSettings = updateSettings,
+                    )
+                }
             }
         }
     }
