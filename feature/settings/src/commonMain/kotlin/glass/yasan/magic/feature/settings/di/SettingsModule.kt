8a5ef5f05b310798f2b494222e33fa4cc9ad0af0
Yasan Glass <yasanglass@gmail.com>
2026-01-14 21:01:24 +0100

    Fix settings not being observable on wasm

--- feature/settings/src/commonMain/kotlin/glass/yasan/magic/feature/settings/di/SettingsModule.kt
+++ feature/settings/src/commonMain/kotlin/glass/yasan/magic/feature/settings/di/SettingsModule.kt
@@ -1,7 +1,9 @@
 package glass.yasan.magic.feature.settings.di
 
+import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.ObservableSettings
 import com.russhwolf.settings.Settings
+import com.russhwolf.settings.observable.makeObservable
 import glass.yasan.magic.feature.settings.data.SettingsMapper
 import glass.yasan.magic.feature.settings.data.SettingsRepositoryImpl
 import glass.yasan.magic.feature.settings.domain.repository.SettingsRepository
@@ -9,8 +11,9 @@ import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
+@OptIn(ExperimentalSettingsApi::class)
 val settingsModule = module {
-    single<ObservableSettings> { Settings() as ObservableSettings }
+    single<ObservableSettings> { Settings().makeObservable() }
     singleOf(::SettingsMapper)
     singleOf(::SettingsRepositoryImpl) { bind<SettingsRepository>() }
 }
